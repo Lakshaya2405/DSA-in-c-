25e4d273f93c0b00ee1a46d5ca63f862ffb2e051
Lakshaya Kumar <kumarlakshaya968@gmail.com>
2026-07-29 01:01:19 +0530

    Time: 0 ms (100%), Space: 12.5 MB (7.82%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -4,17 +4,14 @@ public:
         int indx = m+n-1,i = m-1,j = n-1;
         while(i>=0 && j>=0){
             if(nums1[i] >= nums2[j]){
-                nums1[indx] = nums1[i];
-                indx--,i--;
+                nums1[indx--] = nums1[i--];
             }
             else{
-                nums1[indx] = nums2[j];
-                indx--,j--;
+                nums1[indx--] = nums2[j--];
             }
         }
         while(j>=0){
-            nums1[indx] = nums2[j];
-            indx--,j--;
+            nums1[indx--] = nums2[j--];
         }
     }
 };
