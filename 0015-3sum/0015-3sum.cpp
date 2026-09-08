dd0e355589ce880d3a4c540926d4ad7fea6ef0d5
Lakshaya Kumar <kumarlakshaya968@gmail.com>
2026-09-08 22:08:15 +0530

    Time: 43 ms (82.72%), Space: 29.3 MB (28.5%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
-        for (int p = 0; p < nums.size() - 2; p++) {
-            if (p > 0 && nums[p] == nums[p - 1])
+        vector<vector<int>> res;
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i] == nums[i-1]){
                 continue;
-            int q = p + 1;
-            int r = nums.size() - 1;
-            while (q < r) {
-                int sum = nums[p] + nums[q] + nums[r];
-                if (sum == 0) {
-                    ans.push_back({nums[p], nums[q], nums[r]});
-                    while (q < r && nums[q] == nums[q + 1])
-                        q++;
-                    while (q < r && nums[r] == nums[r - 1])
-                        r--;
-                    q++;
-                    r--;
+            }
+            int sum = -1*nums[i];
+            int left = i+1;
+            int right = n-1;
+            while(left<right){
+                int s = nums[left]+nums[right];
+                if(s == sum){
+                    res.push_back({nums[i],nums[left],nums[right]});
+                    left++;
+                    right--;
+                    while(left<n && nums[left] == nums[left-1]){
+                        left++;
+                    }
+                    while(right>0 && nums[right] == nums[right+1]){
+                        right--;
+                    }
                 }
-                else if (sum < 0) {
-                    q++;
+                else if(s<sum){
+                    left++;
                 }
-                else {
-                    r--;
+                else{
+                    right--;
                 }
             }
         }
-      return ans;
+        return res;
     }
 };
